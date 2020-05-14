9b2b884837aeaa431c7657e8e835edbaf39555de
Irem Nasir <iremn@scripps.edu>
2020-05-14 10:33:09 +0200

    Trying to incorporate ATLAS db for deploying Heroku

--- scripts/train.py
+++ scripts/train.py
@@ -6,7 +6,8 @@ import os
 import re
 
 #Mongo DB config
-client = pymongo.MongoClient()
+#client = pymongo.MongoClient()
+client = pymongo.MongoClient("mongodb+srv://irem:<password>@cluster0-hzvy2.mongodb.net/BioReco?retryWrites=true&w=majority")
 db = client.BioReco
 
 def read_category_meta_df(category):
